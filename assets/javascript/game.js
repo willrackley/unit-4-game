f781fe03b6a7aba24425c6933b63169042e52936
williamrackley <willrackley65@gmail.com>
2018-12-09 14:34:36 -0500

    psuedo coded for the crystal collector assignment

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//psuedo code
+//you need a random number displayed for each game that the user needs to match
+//need for crystals that have different random numbers for everytime you play the game
+//the the user's crystal count needs to increment by the particular random number every time the crystal is clicked. (the user cannot see the number)
+//if the users crystal count matches the computer score to match then the user wins, if he goes over then he loses
+//once the players wins or loses, the game automatically restarts
+//after every win and loss the win/loss counter increments by one
